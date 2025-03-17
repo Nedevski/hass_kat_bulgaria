cd5853c6ae295d77a53872aad714fbf3cc72724d
Nikola Nedevski <nikola.nedevski@gmail.com>
2025-03-17 22:51:41 +0200

    moved update interval to constants

--- custom_components/kat_bulgaria/const.py
+++ custom_components/kat_bulgaria/const.py
@@ -1,5 +1,7 @@
 """Constants for the KAT Bulgaria integration."""
 
+from datetime import timedelta
+
 DOMAIN = "kat_bulgaria"
 
 CONF_PERSON_EGN = "egn"
@@ -7,3 +9,5 @@ CONF_DRIVING_LICENSE = "driving_license_number"
 CONF_PERSON_NAME = "person_name"
 
 COORD_DATA_KEY = "obligations"
+
+DEFAULT_POLL_INTERVAL = timedelta(minutes=30)
