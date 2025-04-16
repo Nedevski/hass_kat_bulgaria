6c9378eb8a56d54662e11e3bfb9d0aa8cf067e1d
Nikola Nedevski <nikola.nedevski@gmail.com>
2025-04-16 16:36:40 +0000

    Added migration tests

---
+++ tests/test_migration.py
@@ -0,0 +1,33 @@
+"""Test migrating from v1 to v2."""
+
+import pytest
+
+from homeassistant.config_entries import ConfigEntryState
+from homeassistant.core import HomeAssistant
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from . import (
+    EGN_VALID,
+    MOCK_DATA_INDIVIDUAL_FULL,
+)
+
+
+@pytest.mark.asyncio
+async def test_init_migration_from_v1(
+    hass: HomeAssistant,
+    config_entry_v1: MockConfigEntry,
+    mock_get_obligations_ok_nodata
+) -> None:
+    """Test migration from v1 to v2."""
+
+    config_entry_v1.add_to_hass(hass)
+
+    await hass.config_entries.async_setup(config_entry_v1.entry_id)
+    await hass.async_block_till_done()
+
+    assert len(hass.config_entries.flow.async_progress()) == 0
+    assert config_entry_v1.state is ConfigEntryState.LOADED
+    assert config_entry_v1.version == 2
+    assert config_entry_v1.unique_id == EGN_VALID
+    assert config_entry_v1.data == MOCK_DATA_INDIVIDUAL_FULL
